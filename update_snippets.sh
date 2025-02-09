afb56062a2ab4cfb4dc30617912b34217703bf38
Ivan Sidorov <vanosidor@gmail.com>
2025-02-09 18:08:50 +0300

    fix last commit

---
+++ update_snippets.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# update snippets in repo (copy from Android Studio)
+# chmod +x update_snippets.sh
+cd ~/Library/Application\ Support/Google/AndroidStudio2024.1/templates && cp Flutter\ snippets.xml ~/Flutter\ live\ templates/
